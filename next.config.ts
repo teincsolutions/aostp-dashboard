11112ee79e513d28ea7760954cd8897e5e69b6da
Eric Mensah <eric.m4511@gmail.com>
2025-09-27 20:30:32 +0000

    fix(Jenkinsfile): setting up for deployment

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'standalone',
   env: {
-    NEXT_PUBLIC_API_BASE_URL:  '__NEXT_PUBLIC_API_BASE_URL__',
+    NEXT_PUBLIC_API_BASE_URL:  'https://api.akomapacargo.com/api/v1',
     NEXT_PUBLIC_GOOGLE_CLIENT_ID: '__NEXT_PUBLIC_GOOGLE_CLIENT_ID__',
   },
 };
